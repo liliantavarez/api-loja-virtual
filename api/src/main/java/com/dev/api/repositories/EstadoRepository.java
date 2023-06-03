f3bb15b0844d40d080e3d94733bc1c830762a735
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2023-06-03 17:05:37 -0300

    feat: repositorio estado

---
+++ api/src/main/java/com/dev/api/repositories/EstadoRepository.java
@@ -0,0 +1,7 @@
+package com.dev.api.repositories;
+
+import com.dev.api.entities.Estado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EstadoRepository extends JpaRepository<Estado, Long> {
+}
