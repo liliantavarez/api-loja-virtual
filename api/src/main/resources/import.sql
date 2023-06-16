55309d8ad498cfef3e7898853fb2047f75a5b44b
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2023-06-16 09:40:08 -0300

    feat: import para popular base de dados com estados

---
+++ api/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into estado(nome, sigla, data_criacao) values('Pernambuco', 'PE', '2023-06-03T18:11:18.151+00:00');
+insert into estado(nome, sigla, data_criacao) values('Paraiba', 'PB', '2023-06-03T18:21:28.151+00:00');
+insert into estado(nome, sigla, data_criacao) values('Ceara', 'CE', '2023-06-03T18:31:38.151+00:00');
