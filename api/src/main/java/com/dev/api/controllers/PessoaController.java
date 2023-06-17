030ab3739d2bd8861d8b46fea78d41358727fbdb
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2023-06-17 12:06:04 -0300

    feat: consumindo serviços da entidade pessoa no controller

---
+++ api/src/main/java/com/dev/api/controllers/PessoaController.java
@@ -0,0 +1,48 @@
+package com.dev.api.controllers;
+
+import com.dev.api.dto.PessoaDTO;
+import com.dev.api.entities.Pessoa;
+import com.dev.api.services.PessoaService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/pessoa")
+public class PessoaController {
+
+    @Autowired
+    private PessoaService pessoaService;
+
+    @GetMapping
+    ResponseEntity<List<PessoaDTO>> buscarTodos() {
+        return ResponseEntity.ok(pessoaService.buscarTodos());
+    }
+
+    @PostMapping
+    ResponseEntity<Pessoa> inserir(@Valid @RequestBody PessoaDTO pessoaDTO, UriComponentsBuilder uriComponentsBuilder) {
+        Pessoa pessoa = pessoaService.inserir(pessoaDTO);
+
+        var uri = uriComponentsBuilder.path("/pessoa/{id}").buildAndExpand(pessoa.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(pessoa);
+    }
+
+    @PatchMapping("/{id}")
+    ResponseEntity<Pessoa> atualizar(@Valid @RequestBody PessoaDTO pessoaDTO, @PathVariable Long id) {
+        Pessoa pessoaAtualizada = pessoaService.atualizar(pessoaDTO, id);
+
+        return ResponseEntity.ok(pessoaAtualizada);
+    }
+
+    @DeleteMapping("/{id}")
+    ResponseEntity<Void> deletar(@PathVariable Long id) {
+        pessoaService.remover(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
