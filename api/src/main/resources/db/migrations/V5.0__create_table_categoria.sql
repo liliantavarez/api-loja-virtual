edf66588832ef1a844367fd8968154d93f98a5ef
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2023-07-15 11:17:04 -0300

    feat: migration para criação de tabela de categoria no banco

---
+++ api/src/main/resources/db/migrations/V5.0__create_table_categoria.sql
@@ -0,0 +1,7 @@
+create table categoria
+(
+    id               bigint       not null auto_increment primary key,
+    nome             varchar(250) not null,
+    data_criacao     datetime,
+    data_atualizacao datetime
+)
