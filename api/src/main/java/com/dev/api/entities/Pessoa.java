7b3baba8158ac2a90e9aa76f02279f1756f09d73
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2023-07-15 11:04:42 -0300

    refactor: incluindo validações na entidade de pessoa

--- api/src/main/java/com/dev/api/entities/Pessoa.java
+++ api/src/main/java/com/dev/api/entities/Pessoa.java
@@ -1,7 +1,11 @@
 package com.dev.api.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import org.hibernate.validator.constraints.br.CPF;
 
 import java.util.Date;
 
@@ -12,11 +16,24 @@ public class Pessoa {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @NotBlank
     private String nome;
+    @NotBlank
+    @Size(min = 11, max = 14)
+    @CPF(message = "CPF inválido")
     private String cpf;
+    @NotBlank
+    @Size(min = 6, max = 120)
+    @Email(message = "E-mail inválido")
     private String email;
+    @NotBlank
+    @Size(min = 8)
     private String senha;
+    @NotBlank
+    @Size(min = 5)
     private String endereco;
+    @NotBlank
+    @Size(min = 8, max = 9, message = "CEP inválido")
     private String cep;
     @ManyToOne
     @JoinColumn(name = "cidade_id")
