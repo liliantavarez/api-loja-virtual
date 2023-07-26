4d1fc6074a0e1703099b44dcae0a26183e494e9f
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2023-07-26 17:10:09 -0300

    refactor: removendo validações dos dtos

--- api/src/main/java/com/dev/api/dto/EstadoDTO.java
+++ api/src/main/java/com/dev/api/dto/EstadoDTO.java
@@ -1,18 +1,9 @@
 package com.dev.api.dto;
 
-import jakarta.annotation.Nonnull;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
-import org.hibernate.validator.constraints.Length;
 
 @Data
 public class EstadoDTO {
-    @NotBlank(message = "Insira o nome do estado!")
-    @Nonnull
-    @Length(min = 4, max = 25)
     private String nome;
-    @NotBlank(message = "Insira a sigla do estado!")
-    @Nonnull
-    @Length(min = 2, max = 2)
     private String sigla;
 }
--- api/src/main/java/com/dev/api/dto/PessoaDTO.java
+++ api/src/main/java/com/dev/api/dto/PessoaDTO.java
@@ -1,33 +1,15 @@
 package com.dev.api.dto;
 
 import com.dev.api.entities.Cidade;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
-import org.hibernate.validator.constraints.br.CPF;
 
 @Data
 public class PessoaDTO {
-
-    @NotBlank
     private String nome;
-    @NotBlank
-    @Size(min = 11, max = 14)
-    @CPF(message = "CPF inválido")
     private String cpf;
-    @NotBlank
-    @Size(min = 6, max = 120)
-    @Email(message = "E-mail inválido")
     private String email;
-    @NotBlank
-    @Size(min = 8)
     private String senha;
-    @NotBlank
-    @Size(min = 5)
     private String endereco;
-    @NotBlank
-    @Size(min = 8, max = 9, message = "CEP inválido")
     private String cep;
     private Cidade cidade;
 }
