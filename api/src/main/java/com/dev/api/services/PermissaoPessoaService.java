e040f27dd766004d113daca1343049364d26d9c3
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2023-07-28 09:01:48 -0300

    feat: cadastro de pessoa com permissão do tipo cliente

---
+++ api/src/main/java/com/dev/api/services/PermissaoPessoaService.java
@@ -0,0 +1,29 @@
+package com.dev.api.services;
+
+import com.dev.api.entities.Permissao;
+import com.dev.api.entities.PermissaoPessoa;
+import com.dev.api.entities.Pessoa;
+import com.dev.api.repositories.PermissaoPessoaRepository;
+import com.dev.api.repositories.PermissaoRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class PermissaoPessoaService {
+
+    private final PermissaoPessoaRepository permissaoPessoaRepository;
+    private final PermissaoRepository permissaoRepository;
+
+    public void vincularPessoaPermissoaCliente(Pessoa pessoa) {
+        List<Permissao> listaPermissao = permissaoRepository.findByNome("Cliente");
+        if (!listaPermissao.isEmpty()) {
+            PermissaoPessoa permissaoPessoa = new PermissaoPessoa();
+            permissaoPessoa.setPessoa(pessoa);
+            permissaoPessoa.setPermissao(listaPermissao.get(0));
+            permissaoPessoaRepository.save(permissaoPessoa);
+        }
+    }
+}
